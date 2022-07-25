1ffa0fede175ef59e14cd9cfb406f6a1140e95fd
Dmitriy Vavilin <dv.vavilin@mail.ru>
2022-07-25 17:36:49 +0300

    third commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        question: 'Какой язык работает в браузере?',
+        answer: ['Java', 'JavaScript', 'Python', 'C++'],
+        corrent: 4,
+    },
+    {
+        question: 'Что означает Css?',
+        answer: [
+            'Central Style Sheets',
+            'Cascading Style Sheets',
+            'Cascading Simple Sheets',
+            'Cars SUVs Sailboats'
+        ],
+        corrent: 2,
+    },
+    {
+        question: 'Что означает Html?',
+        answer: [
+            'Hypertext Markup Languae',
+            'Hupertext Mardown Language',
+            'Hyperloor Machine Language',
+            'Helicopters Terminals Motorboats Lamborginis',
+        ],
+        corrent: 1,
+    },
+    {
+        question: 'В каком году был создан JavaScript',
+        answer: ['1996', '1995', '1994', 'Все ответы не верны!'],
+        corrent: 2,
+    }
+]
